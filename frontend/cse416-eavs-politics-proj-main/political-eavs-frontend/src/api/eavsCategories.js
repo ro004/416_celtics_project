d7032da70c79a62b10c1079140e2f291be5b4376
Cashal17 <kushtr17@gmail.com>
2025-12-15 22:22:38 -0500

    removed underscore from endpoints

--- frontend/cse416-eavs-politics-proj-main/political-eavs-frontend/src/api/eavsCategories.js
+++ frontend/cse416-eavs-politics-proj-main/political-eavs-frontend/src/api/eavsCategories.js
@@ -8,18 +8,18 @@ export async function getProvBallotSummaryandRegionsList(stateFips, year) {
 
 // get active voter summary (GUI-7) alongside active voter counts by region (GUI-8)
 export async function getActiveVoterSummaryandRegionsList(stateFips, year) {
-	const { data } = await api.get(`/eavs/active_voters/${stateFips}/${year}`);
+	const { data } = await api.get(`/eavs/activevoters/${stateFips}/${year}`);
 	return data;
 }
 
 // get pollbook deletion summary (GUI-9) alongside deletion counts by region (GUI-10)
 export async function getPollbookDeletionSummaryandRegionsList(stateFips, year) {
-	const { data } = await api.get(`/eavs/pollbook_deletions/${stateFips}/${year}`);
+	const { data } = await api.get(`/eavs/pollbookdeletions/${stateFips}/${year}`);
 	return data;
 }
 
 // get mail reject summary alongside mail reject counts by region
 export async function getMailRejectSummaryandRegionsList(stateFips, year) {
-	const { data } = await api.get(`/eavs/mail_rejects/${stateFips}/${year}`);
+	const { data } = await api.get(`/eavs/mailrejects/${stateFips}/${year}`);
 	return data;
 }
